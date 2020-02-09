4297804880204014f367698ee06c6539fe339d00
unknown <kumarnigam28@gmail.com>
2020-02-09 12:24:03 +0530

    merge

--- src/component/cart/priceDetails.js
+++ src/component/cart/priceDetails.js
@@ -27,15 +27,18 @@ class ItemsCart extends Component {
 
     render() {
       let total = 0;
+      let discount = 0;
       let pHtml = []
       {this.state.priceArray.map((item,index)=>{
         total = total+item.price
+        discount = item.price - discount
         pHtml.push(<p key={index}>Price ({item.name}) <span>: <FaRupeeSign /> {item.price}</span></p>)
       })}
     return (
         <div className="price-details">
           <h2>Price Details</h2>
           {pHtml}
+          <p>Discount <span>: <FaRupeeSign /> {discount}</span></p>
           <h3>Total Payable <span>:<FaRupeeSign /> {total}</span></h3>
         </div>
     );
