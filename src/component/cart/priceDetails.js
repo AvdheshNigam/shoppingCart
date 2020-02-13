f46dd81a1654dbcc79615caf5eced6db68c92f47
unknown <kumarnigam28@gmail.com>
2020-02-13 17:46:01 +0530

    cart

--- src/component/cart/priceDetails.js
+++ src/component/cart/priceDetails.js
@@ -12,13 +12,14 @@ class ItemsCart extends Component {
       let totalDiscount = 0;
       let pHtml = []
       let dHtml = [...pHtml]
+      
       console.log(dHtml)
       {this.props.priceArray.map((item,index)=>{
         total = total+item.price
         totalDiscount = totalDiscount + item.discount
           pHtml.push(<div>
-                    <p key={index}>Price ({item.quantity +' '+ item.name}) <span>: <FaRupeeSign /> {item.price + item.discount * item.quantity} </span></p>
-                    <p>Discount ({totalDiscount + ' X ' + item.quantity})<span>: <FaRupeeSign /> {item.quantity*(item.discount+item.discount)}</span></p>
+                    <p key={index}>Price ({item.quantity +' '+ item.name}) <span>: <FaRupeeSign /> {(item.price + item.discount) * item.quantity} </span></p>
+          <p>Discount ({totalDiscount + ' X ' + item.quantity})<span>: <FaRupeeSign /> {(item.price+item.discount - item.price) * item.quantity}</span></p>
                     
                   </div>
                   )
